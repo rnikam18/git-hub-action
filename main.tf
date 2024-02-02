693f2ad4dff1e8efe5c5e7fb0bf6c1f7c200a02f
Rohit Nikam <Rohit.Nikam@citrix.com>
2024-02-02 15:09:01 +0530

    Intial comit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.88.0"
+    }
+  }
+}
+
+
+locals {
+  resource_group_name="appgroup"
+  location="Central India"
+}
+
+resource "azurerm_resource_group" "appgroup" {
+name = "appgroup"
+location = "Central India"
+}
+
+resource "" "name" {
+  
+}
+
